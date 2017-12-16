bf912141da120ae67ccc276d6842730628469ce4
mmarcikk <martachimiak@gmail.com>
2017-12-16 14:30:31 +0100

    more inteligent login and logout function

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -1,12 +1,13 @@
 from model.contact import Contact
 
 
-def test_edit_contact(app):
-    app.session.login(username="admin", password="secret")
-    app.contact.edit(Contact(firstname="change", middlename="change", lastname="change",
-                                   nickname="change", title="change", company="change", address="ul. Dluga 55 Change",
-                                   home="111111111", mobile="222222222", work="333333333", fax="444444444",
-                                   email="martachimiak+change@gmail.com", email2="test+change@gmail.com",
-                                   email3="test2+change@gmail.com", homepage="www.change.pl", byear="1990",
-                                   address2="ul. Kręta 66, Change", ayear="2011", phone2="Germany", notes="change"))
-    app.session.logout()
+def test_edit_first_contact_title(app):
+    app.contact.edit_first_contact(Contact(title="change"))
+
+
+def test_edit_first_contact_firstname(app):
+    app.contact.edit_first_contact(Contact(firstname="change"))
+
+
+def test_edit_first_contact_email(app):
+    app.contact.edit_first_contact(Contact(email="martachimiak+change@gmail.com"))
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,7 +1,13 @@
 from model.group import Group
 
 
-def test_edit_group(app):
-    app.session.login(username="admin", password="secret")
-    app.group.edit(Group(name="change",  header="change", footer="change"))
-    app.session.logout()
+def test_edit_first_group_name(app):
+    app.group.edit_first_group(Group(name="change"))
+
+
+def test_edit_first_group_header(app):
+    app.group.edit_first_group(Group(header="change"))
+
+
+def test_edit_first_group_footer(app):
+    app.group.edit_first_group(Group(footer="change"))
