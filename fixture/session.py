427e755ef9482ea96bf0921af716915992eda0db
mmarcikk <martachimiak@gmail.com>
2017-12-17 13:06:18 +0100

    repleaced find_element with find_elements

--- fixture/session.py
+++ fixture/session.py
@@ -16,11 +16,11 @@ class SessionHelper:
 
     def logout(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("Logout").click()
+        wd.find_elements_by_link_text("Logout")[0].click()
 
     def is_logged_in(self):
         wd = self.app.wd
-        return len(wd.find_element_by_link_text("Logout")) > 0
+        return len(wd.find_elements_by_link_text("Logout")) > 0
 
     def is_logged_in_as(self, username):
         wd = self.app.wd
