03a7f26e288f28bacb26718e19cb5f9830a3d256
mmarcikk <martachimiak@gmail.com>
2018-01-02 13:44:38 +0100

    possibility of running tests in different browsers

--- fixture/application.py
+++ fixture/application.py
@@ -1,17 +1,25 @@
+from selenium import webdriver
 from fixture.contact import ContactHelper
 from fixture.group import GroupHelper
 from fixture.session import SessionHelper
-from selenium.webdriver.firefox.webdriver import WebDriver
 
 
 class Application:
 
-    def __init__(self):
-        self.wd = WebDriver(capabilities={"marionette": False},
-            firefox_binary="C:/Program Files/Mozilla Firefox/firefox.exe")
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox(capabilities={"marionette": False},
+                firefox_binary="C:/Program Files/Mozilla Firefox/firefox.exe")
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -23,7 +31,7 @@ class Application:
     def open_home_page(self):
         wd = self.wd
         if not (wd.current_url.endswith("/addressbook") and len(wd.find_elements_by_name("add")) > 0):
-            wd.get("http://localhost:8080/addressbook/")
+            wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
