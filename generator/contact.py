281c18cd940611b79bdb8b2349552f8522b56ffd
mmarcikk <martachimiak@gmail.com>
2018-01-05 15:22:24 +0100

    generating data to/from json file

--- generator/contact.py
+++ generator/contact.py
@@ -1,6 +1,6 @@
 from model.contact import Contact
 import getopt
-import json
+import jsonpickle
 import os.path
 import random
 import string
@@ -37,4 +37,5 @@ testdata = [Contact(firstname="", lastname="", home="", mobile="", work="", phon
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
 with open(file, "w") as out:
-    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
+    jsonpickle.set_encoder_options("json", indent=2)
+    out.write(jsonpickle.encode(testdata))
